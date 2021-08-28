02b6b9d5a8e3672ddca9ea40ac05d1f894aa832e
Rizky Aldiansyah <89493163+RizkyAldiansyah@users.noreply.github.com>
2021-08-28 21:07:53 +0700

    deklasrasi dan cin atau cinput

---
+++ e8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a;
+	a = 1;
+	cout << a << endl;
+
+	int b;
+	cout << "masukan nilai : ";
+	cin >> b;
+	cout << "nilai yang anda masukan adalah : ";
+	cout << b;
+
+	cin.get();
+	return 0;
+}
